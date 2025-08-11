ce9455d607a4e52c7f6551150eac25084bcb8c17
rzkysyftra <riskisyafutrajaya@gmail.com>
2025-08-12 03:37:45 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,66 +1,24 @@
-import type { NextConfig } from 'next';
+import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  poweredByHeader: false,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.yourdomain.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  async redirects() {
-    return [
-      {
-        source: '/index.html',
-        destination: '/',
-        permanent: true,
-      },
-    ];
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin',
-          },
-          {
-            key: 'Permissions-Policy',
-            value:
-              'geolocation=(), microphone=(), camera=(), fullscreen=(self)',
-          },
-        ],
-      },
-    ];
-  },
+/* config options here */
+typescript: {
+ignoreBuildErrors: true,
+},
+eslint: {
+ignoreDuringBuilds: true,
+},
+images: {
+remotePatterns: [
+{
+protocol: 'https',
+hostname: 'placehold.co',
+port: '',
+pathname: '/**',
+},
+],
+},
 };
 
 export default nextConfig;
+
